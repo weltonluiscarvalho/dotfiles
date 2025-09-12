05a98ffe0e363b57e49cdc5ae2476c81c8aa07fa
Welton Carvalho <welton.carvalho.dev@gmail.com>
2025-09-12 09:53:02 -0300

    feat: define tab with 4 spaces in all kind of files opened in neovim

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,6 +1,10 @@
 require "nvchad.options"
 
 -- add yours here!
-
+local opt = vim.opt
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.softtabstop = 4
+opt.expandtab = true
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
